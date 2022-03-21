634ec3d643ef01a9063d647bfe83ec3808344a1d
KEvans254 <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@github.com>
2022-03-21 10:27:37 +0300

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+	int count = 0;
+	
+	_putchar('\n')
+	do
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	while (str[count] != '\0')
+}
